be138a0fb65e15d3907a80a754fa9ce4bcf4f818
Andtiy <varyshko706@gmail.com>
2024-02-05 17:31:44 +0200

    Super final

--- task1/app.js
+++ task1/app.js
@@ -1,2 +1,19 @@
 'use strict'
 
+const minus = document.querySelector('.counter__minus');
+const plus = document.querySelector('.counter__plus');
+const val = document.querySelector('.counter__value');
+
+let c = +val.textContent;
+
+plus.addEventListener('click', function() {
+    val.textContent = ++c;
+});
+
+minus.addEventListener('click', function() {
+    val.textContent = --c;
+})
+
+
+
+
